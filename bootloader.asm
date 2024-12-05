ebdfc80c4b7d0a863cbb63729a3871aa3a5ab3df
achaisa <achaisa@unsa.edu.pe>
2024-12-05 10:27:49 -0500

    Prueba 2 : Qemu Kernel C, no se imprime el H

--- bootloader.asm
+++ bootloader.asm
@@ -1,12 +1,14 @@
 bits 16
 org 0x7c00
 
-mov ah, 0x0E   ; Modo de impresión en pantalla
-mov al, 'H'    ; Caracter 'H'
-int 0x10       ; Llamada a la BIOS para imprimir
+; Mostrar un mensaje
+mov ah, 0x0E
+mov al, 'H'
+int 0x10
 
-cli            ; Desactiva interrupciones
-hlt            ; Detiene la CPU
+; Saltar al kernel
+jmp 0x1000
 
-times 510-($-$$) db 0  ; Rellena hasta 512 bytes
+times 510-($-$$) db 0  ; Rellenar con ceros hasta 512 bytes
 dw 0xAA55              ; Firma de arranque
+
--- kernel.c
+++ kernel.c
@@ -1,7 +1,18 @@
 void main() {
-    char *video_memory = (char *)0xb8000;
-    *video_memory = 'H';
-    *(video_memory + 1) = 0x07;
+    volatile char *video_memory = (char *)0xb8000;
+
+    // Escribe "HELLO" en la parte superior de la pantalla
+    const char *message = "HELLO";
+    for (int i = 0; message[i] != '\0'; i++) {
+        video_memory[i * 2] = message[i];        // Caracter
+        video_memory[i * 2 + 1] = 0x0F;         // Atributo (blanco brillante sobre negro)
+    }
+
+    while (1) {}  // Bucle infinito
+}
+
+void _start() {
+    main();
     while (1) {}
 }
 
