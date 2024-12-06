050d405083599924be5e5c6e347309dfddea0a6d
achaisa <achaisa@unsa.edu.pe>
2024-12-05 22:02:03 -0500

    Prueba 3 - Probando el primer 'schedule' basico RR

--- kernel.c
+++ kernel.c
@@ -1,18 +1,37 @@
-void main() {
-    volatile char *video_memory = (char *)0xb8000;
-
-    // Escribe "HELLO" en la parte superior de la pantalla
-    const char *message = "HELLO";
-    for (int i = 0; message[i] != '\0'; i++) {
-        video_memory[i * 2] = message[i];        // Caracter
-        video_memory[i * 2 + 1] = 0x0F;         // Atributo (blanco brillante sobre negro)
-    }
-
-    while (1) {}  // Bucle infinito
+void proceso1() {
+    char *video_memory = (char *)0xb8000;
+    *video_memory = 'A';       // Letra 'A'
+    *(video_memory + 1) = 0x07;
 }
 
-void _start() {
-    main();
+void proceso2() {
+    char *video_memory = (char *)0xb8002;
+    *video_memory = 'B';       // Letra 'B'
+    *(video_memory + 1) = 0x07;
+}
+
+void proceso3() {
+    char *video_memory = (char *)0xb8004;
+    *video_memory = 'C';       // Letra 'C'
+    *(video_memory + 1) = 0x07;
+}
+
+typedef void (*Proceso)();
+Proceso procesos[] = {proceso1, proceso2, proceso3};
+int num_procesos = 3;
+
+void scheduler() {
+    int i = 0;
+    while (1) {
+        procesos[i]();  // Ejecuta el proceso actual
+        for (volatile int j = 0; j < 100000; j++) {} // Retraso
+        i = (i + 1) % num_procesos;  // Alterna al siguiente proceso
+    }
+}
+
+void main() {
+    char *video_memory = (char *)0xb8000;
+    *video_memory = 'K';  // Indica que el kernel ha iniciado
     while (1) {}
 }
 
