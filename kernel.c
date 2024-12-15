a621a15342c3a7224ba2b6b5985f4c03d39a02d0
achaisa <achaisa@unsa.edu.pe>
2024-12-15 13:08:54 -0500

    Creando el planificador basico inicial

--- kernel.c
+++ kernel.c
@@ -31,3 +31,34 @@ void ordenar_por_prioridad(proceso *tabla, int tamano) {
         }
     }
 }
+
+void planificador() {
+    ordenar_por_prioridad(tabla_procesos, MAX_PROCESOS); 
+    bool todos_terminados = false;
+
+    while (!todos_terminados) {
+        todos_terminados = true;
+
+        for (int i = 0; i < MAX_PROCESOS; i++) {
+            if (tabla_procesos[i].tiempo_restante > 0) {
+	 	//Todavia tenemos procesos
+                todos_terminados = false; 
+
+                // simulacion
+                __builtin_memcpy(tabla_procesos[i].estado, "ejecutando", 11);
+                int tiempo_ejecutado = (tabla_procesos[i].tiempo_restante > CUANTO_TIEMPO) ? CUANTO_TIEMPO : tabla_procesos[i].tiempo_restante;
+                tabla_procesos[i].tiempo_restante -= tiempo_ejecutado;
+
+                // simu tiempo de ejecucion (retardo)
+                for (int t = 0; t < 1000000; t++); 
+
+                // actualizacion dele stado actual
+                if (tabla_procesos[i].tiempo_restante == 0) {
+                    __builtin_memcpy(tabla_procesos[i].estado, "terminado", 9);
+                } else {
+                    __builtin_memcpy(tabla_procesos[i].estado, "listo", 5);
+                }
+            }
+        }
+    }
+}
