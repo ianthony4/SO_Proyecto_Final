7bca8a4e29cd70075496ca7b5b391431cf50d4db
achaisa <achaisa@unsa.edu.pe>
2024-12-15 13:02:52 -0500

    Creando algunos proceso de prueba

--- kernel.c
+++ kernel.c
@@ -13,3 +13,8 @@ typedef struct {
     char estado[12];        // listo, ejecutando, terminado
 } proceso;
 
+proceso tabla_procesos[MAX_PROCESOS] = {
+    {1, 2, 5, 5, "listo"},
+    {2, 1, 6, 6, "listo"},
+    {3, 3, 4, 4, "listo"}
+};
